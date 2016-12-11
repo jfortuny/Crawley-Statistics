8a127d74233e5e043f648364ac08a612557befe5
jfortuny <jfortuny@jfortuny.com>
2016-12-11 07:18:19 -0500

    completed chapter 6

--- myCode/Chapter06-SingleSamples.R
+++ myCode/Chapter06-SingleSamples.R
@@ -27,3 +27,59 @@ xv <- seq(2,12,0.1)
 yv <- 10000 * dnorm(xv, mean(mean.outcome), sd(mean.outcome))
 hist(mean.outcome, breaks = (1.5:12.5), ylim = c(0,3000), col = "yellow", main = "")
 lines(xv,yv,col="red")
+
+standard.deviations <- seq(-3,3, 0.01)
+pd <- dnorm(standard.deviations)
+plot(standard.deviations, pd, type = "l", col = "blue")
+
+# Using z: Average Height = 170, Standard Deviation of Height = 8
+ht <- seq(150,190,0.01)
+plot(ht, dnorm(ht, mean = 170, sd = 8), type = "l", col = "brown",
+     ylab = "Probability Density", xlab = "Height")
+
+# playing with polygon for graphing
+xv <- seq(0, 10, 0.01)
+yv <- seq(0, 10, 0.01)
+plot(xv, yv, type = "n")
+x <- c(2,6,8)
+y <- c(2,2,8)
+polygon(x,y)
+
+# use of polygon for coloring
+par(mfrow=c(2,2))
+ht <- seq(150,190,0.01)
+pd <- dnorm(ht, mean = 170, sd = 8)
+# top left plot
+plot(ht, dnorm(ht, mean = 170, sd = 8), type = "l", col = "brown",
+     ylab = "Probability Density", xlab = "Height")
+# top right plot
+plot(ht, dnorm(ht, mean = 170, sd = 8), type = "l", col = "brown",
+     ylab = "Probability Density", xlab = "Height")
+yv <- pd[ht <= 160]
+xv <- ht[ht<=160]
+xv <- c(xv, 160, 150)
+yv <- c(yv, yv[1], yv[1])
+polygon(xv, yv, col = "orange")
+# bottom left graph
+plot(ht, dnorm(ht, mean = 170, sd = 8), type = "l", col = "brown",
+     ylab = "Probability Density", xlab = "Height")
+xv <- ht[ht >= 185]
+yv <- pd[ht >= 185]
+xv <- c(xv, 190, 185)
+yv <- c(yv, yv[501], yv[501])
+polygon(xv, yv, col = "blue")
+# bottom left graph
+plot(ht, dnorm(ht, mean = 170, sd = 8), type = "l", col = "brown",
+     ylab = "Probability Density", xlab = "Height")
+xv <- ht[ht >= 160 & ht <= 180]
+yv <- pd[ht >= 160 & ht <= 180]
+xv <- c(xv, 180, 160)
+yv <- c(yv, pd[1], pd[01])
+polygon(xv, yv, col = "green")
+par(mfrow=c(1,1))
+
+# Testing normality
+data <- read.csv("./data/skewdata.csv")
+names(data)
+qqnorm(data$values)
+qqline(data$"values", lty=2)
